d88572e6f8a422d88546cf3ca0e0906490f8ba32
Md Shoaib Abdullah Khan <60256334+Md-Shoaib-Abdullah-Khan@users.noreply.github.com>
2021-04-18 23:51:03 +0600

    Commit on 18-04-2021

---
+++ A. FashionabLee.c
@@ -0,0 +1,17 @@
+    #include<stdio.h>
+    #include<string.h>
+
+    int main()
+    {
+        long long int a,b,c=0,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w;
+        char y[105],z[105];
+        scanf("%lld",&t);
+    for(j=1;j<=t;j++)
+    {c=0;
+    d=0;
+        scanf("%lld",&n);
+    if(n%4==0){printf("YES\n");}
+    else{printf("NO\n");}
+
+    }
+    }
