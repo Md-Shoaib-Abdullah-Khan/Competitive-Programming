1a52ab7eb36144bfd8b18d8d08c6c47aba8f6d3e
Shoaib Khan <mdshoaib.a.khan@gmail.com>
2023-01-13 22:54:16 +0600

    commited on 13-01-2023

--- Codeforces/VS Code/B/tempCodeRunnerFile.cpp
+++ Codeforces/VS Code/B/tempCodeRunnerFile.cpp
@@ -1,8 +1,2 @@
-for(i=0;i<vec1.size();i++){
-    //     sum+=(vec1[i]+increment1);
-    // }
-    // for(i=0;i<vec2.size();i++){
-    //     sum+=(vec2[i]+increment2);
-    // }
-    
-    // cout<<sum<<endl;
+
+        lo(i,2,n+1,2){
